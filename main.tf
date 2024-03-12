bce21d2e2711671f4a77b97b13a1be8373a4ece4
angkb <sgpkokbeng@yahoo.com.sg>
2024-03-12 17:43:54 +0800

    update asw_sqs_queue in main.tf

--- main.tf
+++ main.tf
@@ -34,12 +34,11 @@ resource "aws_s3_bucket_notification" "bucket_notification" {
     id        = "create-object-event"
     queue_arn = aws_sqs_queue.queue.arn
     events    = ["s3:ObjectCreated:*"]
-    # filter_prefix = ".log"
   }
 
   queue {
-    id            = "del-object-event"
-    queue_arn     = aws_sqs_queue.queue.arn
-    events        = ["s3:ObjectRemoved:*"]
-  }  
+    id        = "del-object-event"
+    queue_arn = aws_sqs_queue.queue.arn
+    events    = ["s3:ObjectRemoved:*"]
+  }
 }
--- providers.tf
+++ providers.tf
@@ -1,12 +1,12 @@
 terraform {
   required_providers {
     aws = {
-      source = "hashicorp/aws"
+      source  = "hashicorp/aws"
       version = "5.40.0"
     }
   }
 }
 
 provider "aws" {
-    region = "ap-southeast-1"
+  region = "ap-southeast-1"
 }
